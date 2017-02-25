b827b9536b3aae59f02dc9f23df26162ab67f007
gomholt <gomholt113@gmail.com>
2017-02-24 22:57:22 -0800

    generation of master background still needs work.

--- MotionTracking/main.cpp
+++ MotionTracking/main.cpp
@@ -5,11 +5,25 @@
 #include <queue>
 #include <algorithm>
 
-int thresh_max = 100;
+int thresh_max = 255;//100
 int thresh;
-int box_thresh_max = 100;
+int box_thresh_max = 255;//100
 int box_thresh;
 cv::RNG rng(12345);
+
+//testing trackbars for expiermenting with hsv color filtering
+int H_thresh_max = 180;
+int H_thresh;
+
+int S_thresh_max = 255;
+int S_thresh;
+
+int V_thresh_max = 255;
+int V_thresh;
+
+int HSV_range_max = 50;
+int HSV_range;
+
 
 struct diff {
 	cv::Mat mat;
@@ -19,38 +33,27 @@ struct diff {
 	int colDiffmax = 0;
 };
 
-void calcDiffHue(cv::Mat& prev, cv::Mat& curr, diff& diff) {
+struct background {
+	cv::Mat mat;
+	int** avgPixelCount = new int*[480];
+};
+
+void filterHSV(cv::Mat& prev, cv::Mat& curr, diff& diff) {
 	for (int i = 0; i < prev.rows; ++i) {
 		cv::Vec3b* currPixel = curr.ptr<cv::Vec3b>(i);
 		cv::Vec3b* prevPixel = prev.ptr<cv::Vec3b>(i);
 		cv::Vec3b* diffPixel = diff.mat.ptr<cv::Vec3b>(i);
 
 		for (int j = 0; j < prev.cols; ++j) {
-			
-			diffPixel[j][0] = (currPixel[j][0] > prevPixel[j][0]) ? currPixel[j][0] - prevPixel[j][0] : prevPixel[j][0] - currPixel[j][0];
-			//diffPixel[j][1] = (currPixel[j][1] > prevPixel[j][1]) ? currPixel[j][1] - prevPixel[j][1] : prevPixel[j][1] - currPixel[j][1];
-			//diffPixel[j][2] = (currPixel[j][2] > prevPixel[j][2]) ? currPixel[j][2] - prevPixel[j][2] : prevPixel[j][2] - currPixel[j][2];
+			if ((currPixel[j][0] > (H_thresh - HSV_range) && currPixel[j][0] < (H_thresh + HSV_range))
+				/*&& (currPixel[j][1] > (S_thresh - HSV_range) && currPixel[j][1] < (S_thresh + HSV_range))*/
+				&& (currPixel[j][2] > (V_thresh - HSV_range) && currPixel[j][2] < (V_thresh + HSV_range))) {
 
-			if ((diffPixel[j][0] /*+ diffPixel[j][1] + diffPixel[j][2]*/) > thresh) {
-				std::cout << currPixel[j][0] << " " << prevPixel[j][0] <<" " << diffPixel[j][0] << std::endl;
-				diffPixel[j] = { 255,255,255 };
-				diff.colDifftotals[j] += 1;
-				diff.rowDifftotals[i] += 1;
+				diffPixel[j] = { currPixel[j][0], currPixel[j][1], currPixel[j][2] };
 			}
 			else {
 				diffPixel[j] = { 0,0,0 };
-			}
-		}
-	}
-
-	for (int i = 0; i < prev.rows; ++i) {
-		if (diff.rowDifftotals[i] > diff.rowDiffmax) {
-			diff.rowDiffmax = diff.rowDifftotals[i];
-		}
-	}
-	for (int i = 0; i < prev.cols; ++i) {
-		if (diff.colDifftotals[i] > diff.colDiffmax) {
-			diff.colDiffmax = diff.colDifftotals[i];
+			}			
 		}
 	}
 }
@@ -123,9 +126,38 @@ std::vector<int> motionBox(diff& diff) {
 	return{ left, right, top, bottom };
 }
 
+bool inBox(std::vector<int> box, int x, int y) {
+	if ((x > box[0] && x < box[1]) && (y > box[2] && y < box[3])) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void averageBackground(background& mbkg, cv::Mat& curr, std::vector<int>& roiBox) {
+	for (int i = 0; i < curr.rows; ++i) {
+		
+		cv::Vec3b* currPixel = curr.ptr<cv::Vec3b>(i);
+		cv::Vec3b* mbkgPixel = mbkg.mat.ptr<cv::Vec3b>(i);
+
+		for (int j = 0; j < curr.cols; ++j) {
+			//std::cout << j << "  " << i << std::endl;
+			if (!inBox(roiBox, j,i)) {
+				mbkgPixel[j] = { (uchar)(((mbkgPixel[j][0] * mbkg.avgPixelCount[i][j]) + currPixel[j][0]) / (mbkg.avgPixelCount[i][j] + 1)),
+								(uchar)(((mbkgPixel[j][1] * mbkg.avgPixelCount[i][j]) + currPixel[j][1]) / (mbkg.avgPixelCount[i][j] + 1)),
+								(uchar)(((mbkgPixel[j][2] * mbkg.avgPixelCount[i][j]) + currPixel[j][2]) / (mbkg.avgPixelCount[i][j] + 1)) };
+			}
+			mbkg.avgPixelCount[i][j]++;
+		}
+	}
+}
+
 
 int main(int, char**)
 {
+	
+
 	thresh = 36;
 	box_thresh = 50;
 	cv::VideoCapture cap(1); // open the default camera
@@ -138,12 +170,23 @@ int main(int, char**)
 	cv::namedWindow("color", 1);
 	cv::namedWindow("binary", 1);
 	cv::namedWindow("ROI", 0);
-	cv::resizeWindow("ROI", 640, 480);
-
+	cv::namedWindow("MasterBackground", 1);
 
 	cv::createTrackbar("Threshold Value", "binary", &thresh, thresh_max);
 	cv::createTrackbar("Linethreshold", "color", &box_thresh, box_thresh_max);
+
+	/*cv::createTrackbar("Hue", "ROI", &H_thresh, H_thresh_max);
+	cv::createTrackbar("Saturation", "ROI", &S_thresh, S_thresh_max);
+	cv::createTrackbar("Value", "ROI", &V_thresh, V_thresh_max);
+	cv::createTrackbar("range +-", "ROI", &HSV_range, HSV_range_max);*/
+
 	cap >> prev;
+	background masterBkg;
+	for (int i = 0; i < 480; i++) {
+		masterBkg.avgPixelCount[i] = new int[640];
+	}
+	masterBkg.mat = cv::Mat(prev.size(), prev.type());
+	
 	for (;;)
 	{
 		cv::Mat curr;
@@ -153,13 +196,15 @@ int main(int, char**)
 		diff.mat = cv::Mat(curr.size(), curr.type());
 		cv::GaussianBlur(curr, curr, cv::Size(7, 7), 1.5, 1.5);
 
-		//calcDiffRGB(prev, curr, diff);
+		calcDiffRGB(prev, curr, diff);
 
 		cv::Mat prevHSV, currHSV;
 		cv::cvtColor(curr, currHSV, CV_BGR2HSV);
 		cv::cvtColor(prev, prevHSV, CV_BGR2HSV);
 
-		calcDiffHue(prevHSV, currHSV, diff);
+		//filterHSV(prevHSV, currHSV, diff); 
+		//cv::cvtColor(diff.mat, diff.mat, CV_HSV2BGR);
+
 		curr.copyTo(prev);
 
 		std::vector<int> box;
@@ -226,10 +271,28 @@ int main(int, char**)
 			//std::cout << diff.colDiffmax << " " << diff.rowDiffmax << std::endl;
 			cv::rectangle(curr, bound, CV_RGB(255, 0, 0), 2);
 			cv::rectangle(diff.mat, bound, CV_RGB(255, 0, 0), 2);
+			
 		}
+
+		if (!box.empty()) {
+			averageBackground(masterBkg, curr, box);
+		}
+		else {
+			std::vector<int> temp = { 0,0,0,0 };
+			averageBackground(masterBkg, curr, temp);
+		}
+
 
 		cv::imshow("binary", diff.mat);
 		cv::imshow("color", curr);
+		cv::imshow("MasterBackground", masterBkg.mat);
+
+		//cv::Vec3b rgb = { (uchar)H_thresh,(uchar)V_thresh,(uchar)S_thresh };
+		//cv::Mat tempHSV = cv::Mat(100, 100, CV_8UC3, cv::Scalar(rgb));
+		//cv::Mat tempBGR;
+		//cv::cvtColor(tempHSV, tempBGR, CV_HSV2BGR);
+		//cv::imshow("ROI",tempBGR);
+
 		if (cv::waitKey(5) >= 0) {}
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
